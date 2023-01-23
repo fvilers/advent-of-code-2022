c016548f2a3a8866a9942f179d0304a24b06ab03
Fabian Vilers <fabian.vilers@dev-one.com>
2023-01-23 08:46:59 +0100

    Add challenge resolution for day 01

--- day-01-calorie-counting/src/main.rs
+++ day-01-calorie-counting/src/main.rs
@@ -1,3 +1,29 @@
+// https://adventofcode.com/2022/day/1
+
+use std::fs;
+
+const INPUT_FILE: &str = "input.txt";
+
+#[cfg(windows)]
+const LINE_SEPARATOR: &str = "\r\n";
+#[cfg(not(windows))]
+const LINE_SEPARATOR: &str = "\n";
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string(INPUT_FILE).expect(&format!("Cannot read {INPUT_FILE} file"));
+    let blank_line = LINE_SEPARATOR.to_string() + LINE_SEPARATOR;
+    let groups = input.split(&blank_line);
+    let sums = groups.into_iter().map(|group| {
+        group
+            .split(LINE_SEPARATOR)
+            .into_iter()
+            .map(|line| {
+                line.parse::<i32>()
+                    .expect(&format!("Couldn't parse value '{line}'"))
+            })
+            .sum::<i32>()
+    });
+    let max = sums.max().expect("Couldn't determine the maximum value");
+
+    println!("The Elf carrying the most Calories has {max} Calories");
 }
